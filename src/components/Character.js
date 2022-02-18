67af14fc553aff939202015093d2e0b4b5a2c6f8
Benroberts37 <benrobsemail@gmail.com>
2022-02-17 17:16:34 -0800

    Set up a few intial things, imports, exports, axios, etc.

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React from 'react'
+
+const Character = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )  
+}
+
+
+export default Character;
